2a9f131198196873607f3c13d6a5e5de362bb907
Vaibhav Malhotra <115654765+vaibha-v19@users.noreply.github.com>
2024-11-23 19:05:03 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
       <div className='hidden lg:block h-screen'>
         <img src='./banner.jpeg' alt='Banner' className='h-full rounded-lg ml-0 overflow-hidden' />
       </div>
-      <div className='bg-slate-900 shadow-lg shadow-slate-800 h-[32vh] w-[80vw] md:w-[60vw] lg:w-[64vh] border-spacing-2 rounded-lg flex flex-col items-center justify-center p-6 lg:ml-[350px]'>
+      <div className='bg-slate-900 shadow-lg shadow-slate-800 h-[32vh] w-[90vw] md:w-[60vw] lg:w-[64vh] border-spacing-2 rounded-lg flex flex-col items-center justify-center p-6 lg:ml-[350px]'>
         <h1 className='text-white font-bold text-2xl mt-4 lg:mt-9'>FileShare</h1>
         <p className='text-white text-[18px] mt-2'>Upload file to share!!</p>
         <button onClick={onUploadClick} className='p-2 font-semibold bg-purple-500 text-white rounded-lg hover:bg-white hover:text-purple-500 border-white border-2 shadow-lg mt-4 lg:mt-12'>Upload</button>
